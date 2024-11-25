f2b8274461edbbe9644934337fbd03e5aabfc041
trevooga <nojji@yandex.ru>
2024-11-25 12:08:16 +0300

    reworked MapSchema

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -5,26 +5,26 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Predicate;
 
-public class MapSchema extends BaseSchema<Map<String, Object>> {
-    private final List<Predicate<Map<String, Object>>> checks = new ArrayList<>();
+public class MapSchema<T> extends BaseSchema<Map<String, T>> {
+    private final List<Predicate<Map<String, T>>> checks = new ArrayList<>();
 
-    public MapSchema required() {
+    public MapSchema<T> required() {
         checks.add(map -> map != null);
         return this;
     }
 
-    public MapSchema sizeof(int size) {
+    public MapSchema<T> sizeof(int size) {
         checks.add(map -> map != null && map.size() >= size);
         return this;
     }
 
-    public MapSchema shape(Map<String, BaseSchema<?>> schemas) {
+    public <U> MapSchema<T> shape(Map<String, BaseSchema<U>> schemas) {
         checks.add(map -> {
             for (var entry : schemas.entrySet()) {
                 String key = entry.getKey();
-                BaseSchema<?> schema = entry.getValue();
+                BaseSchema<U> schema = entry.getValue();
                 if (map.containsKey(key)) {
-                    Object value = map.get(key);
+                    T value = map.get(key);
                     if (!schema.isValid(value)) {
                         return false;
                     }
@@ -38,12 +38,12 @@ public class MapSchema extends BaseSchema<Map<String, Object>> {
     }
 
     @Override
-    protected List<Predicate<Map<String, Object>>> getChecks() {
+    protected List<Predicate<Map<String, T>>> getChecks() {
         return checks;
     }
 
     @Override
-    protected Class<Map<String, Object>> getType() {
-        return (Class<Map<String, Object>>) (Class<?>) Map.class;
+    protected Class<Map<String, T>> getType() {
+        return (Class<Map<String, T>>) (Class<?>) Map.class;
     }
 }
